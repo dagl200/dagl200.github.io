e2079c2b52a0f92426651149391404d77bfd84bb
Diego <dgarcialuciano@gmail.com>
2022-04-17 19:30:02 -0400

    Mapa

---
+++ index.js
@@ -0,0 +1,115 @@
+﻿let infoWindow;
+let cord;
+let currentPos;
+let listMarker = ['G35M+2M Santo Domingo','F3CQ+G5 Santo Domingo','F3JP+H4 Santo Domingo']
+function initMap() {
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 18.471953, lng: -69.941496 },
+    mapID: 'mapa 1'
+  });
+
+  map.setOptions({ styles: styles["hide"] });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.BOTTOM].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          currentPos = { ...pos };
+          cord = pos;
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
+  directionsRenderer.setMap(map);
+
+  const onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  };
+
+  document.getElementById("start").addEventListener("change", onChangeHandler);
+  document.getElementById("end").addEventListener("change", onChangeHandler);
+}
+
+const styles = {
+  default: [],
+  hide: [
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.icon",
+      stylers: [{ visibility: "on" }],
+    },
+  ],
+};
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        currentPos = { ...pos };
+
+        directionsService
+    .route({
+      origin: new google.maps.LatLng(currentPos.lat, currentPos.lng),
+      //{
+      //     // query: document.getElementById("start").value,
+
+      // }
+      destination: {
+        query: document.getElementById("end").value,
+      },
+      travelMode: "TRANSIT",
+      transitOptions: {
+        modes: ["TRAIN"],
+        routingPreference: "LESS_WALKING",
+      },
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + status));
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+  
+}
+
